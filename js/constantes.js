154e23c16af053a1b6122bee95f5685c852af3ea
[NandoNoriega] <[noriegafernando@hotmail.com]>
2022-09-20 14:50:17 -0500

    20-09-2022

---
+++ js/constantes.js
@@ -0,0 +1,20 @@
+export const PI= Math.PI;
+
+export let usuario="Nando";
+
+let password ="qwerty";
+//export password;
+
+const hello = () => console.log("H0Ola");
+
+export default function saludar(){
+    console.log("Hola Módulos +ES6")
+}
+
+
+export class Saludar{
+    constructor(){
+        console.log("Hola Clases +ES6")
+    }
+}
+
